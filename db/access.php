4f6c402f7494b5dc9448cebc0b1014716aa76679
Mark Johnson <mark.johnson@tauntons.ac.uk>
2011-09-26 16:44:14 +0100

    Added capabilities

---
+++ db/access.php
@@ -0,0 +1,65 @@
+<?php
+
+$capabilities = array(
+
+    // Create new sessions and generate reviews for classes
+    'moodle/local_progressreview:manage' => array(
+        'captype' => 'write',
+        'riskbitmask' => RISK_MANAGETRUST|RISK_PERSONAL,
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'manager' => CAP_ALLOW
+        )
+    ),
+
+    // View all reviews within a category/department
+    'moodle/local_progressreview:viewall' => array(
+        'captype' => 'read',
+        'riskbitmask' => RISK_PERSONAL,
+        'contextlevel' => CONTEXT_COURSECAT,
+        'archetypes' => array(
+            'manager' => CAP_ALLOW,
+            'coursecreator' => CAP_ALLOW
+        )
+    ),
+
+    // View all reviews within a course
+    'moodle/local_progressreview:view' => array(
+        'captype' => 'read',
+        'riskbitmask' => RISK_PERSONAL,
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => array(
+            'manager' => CAP_ALLOW,
+            'coursecreator' => CAP_ALLOW,
+            'teacher' => CAP_ALLOW
+        )
+    ),
+
+    // View own reviews
+    'moodle/local_progressreview:viewown' => array(
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_USER,
+        'archetypes' => array(
+            'student' => CAP_ALLOW
+        )
+    ),
+
+    // Write reviews within a course
+    'moodle/local_progressreview:write' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => array(
+            'teacher' => CAP_ALLOW
+        )
+    ),
+
+    // Transfer an existing review to a different teacher
+    'moodle/local_progressreview:transfer' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => array(
+            'manager' => CAP_ALLOW,
+            'coursecreator' => CAP_ALLOW
+        )
+    )
+);
