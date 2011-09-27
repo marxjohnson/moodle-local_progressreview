64e104a53a7bcbffa2f4a09dd33138222af66ac0
Mark Johnson <mark.johnson@tauntons.ac.uk>
2011-09-27 09:45:50 +0100

    Created course selector for distributing reviews to courses

--- session.php
+++ session.php
@@ -31,7 +31,30 @@ if (!$id) {
 } else {
     $output = $PAGE->get_renderer('local_progressreview');
     $session = $DB->get_record('progressreview_session', array('id' => $id));
-    $subject_selector = $output->subject_selector($session);
+
+    $potential_subject_selector = new progressreview_potential_course_selector('potential_subjects');
+    $distributed_subject_selector = new progressreview_distributed_course_selector('distributed_subjects', '', $session->id);
+
+    //$potential_subject_selector->exclude(array_keys(current($distributed_subject_selector->find_users())));
+
+    $subjects = array();
+
+    if (optional_param('generate', false, PARAM_TEXT)) {
+        $subjects = $potential_subject_selector->get_selected_users(); 
+    } else if (optional_param('regenerate', false, PARAM_TEXT)) {
+        $subjects = current($distributed_subject_selector->find_users());
+    }
+
+    if ($subjects) {
+        foreach ($subjects as $subject) {
+            progressreview_controller::generate_reviews_for_course($subject->id, $session->id, PROGRESSREVIEW_SUBJECT);
+        }
+        redirect($PAGE->url->out(), get_string('reviewsgenerated', 'local_progressreview'));
+        exit();
+    }
+
+    $subject_selector = $output->course_selector_form($potential_subject_selector, $distributed_subject_selector, $session->id);
+    $content = $subject_selector;
 }
 
 echo $OUTPUT->header();
