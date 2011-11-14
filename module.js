4039daacf1a99b13e10c5ea63191147bf47fbde3
Mark Johnson <mark.johnson@tauntons.ac.uk>
2011-11-14 15:32:08 +0000

    Small JS tweak to appease the IE gods

--- module.js
+++ module.js
@@ -11,7 +11,7 @@ M.local_progressreview = {
         this.progress = Y.one('#progressindicator');
         strautosave = M.util.get_string('autosaving', 'local_progressreview');
         this.progress.one('#autosavelabel').setContent(strautosave);
-        this.progress.hide('fadeOut');
+        this.progress.setStyle('display', 'none');
         this.savebutton = Y.one('#id_save');
         strsaveactive = M.util.get_string('autosaveactive', 'local_progressreview');
         this.savebutton.set('disabled', true);
@@ -22,7 +22,7 @@ M.local_progressreview = {
     autosave: function(plugin, field, value) {
 
         Y = this.Y;
-        this.progress.show('fadeIn');
+        this.progress.setStyle('display', 'none');
         var studentid = Y.one('#id_editid').get('value');
         var sessionid = Y.one('#id_sessionid').get('value');
         var courseid = Y.one('#id_courseid').get('value');
@@ -43,7 +43,7 @@ M.local_progressreview = {
             },
             on: {
                 success: function(id, o) {
-                    M.local_progressreview.progress.hide('fadeOut');
+                    M.local_progressreview.progress.setStyle('display', 'none');
                 },
 
                 failure: function(id, o) {
@@ -52,6 +52,7 @@ M.local_progressreview = {
                     alert(M.util.get_string('autosavefailed', 'local_progressreview', message));
                     module.savebutton.set('disabled', false);
                     module.savebutton.set('value', module.savestring);
+                    M.local_progressreview.progress.setStyle('display', 'none');
                 }
             }
         });
