f30ac1446d7e3839635b0a464078cefcb98936f4
Mark Johnson <mark.johnson@tauntons.ac.uk>
2011-11-15 10:18:51 +0000

    Modified local targetgrades method to only use avgcse in induction review

--- plugins/subject/locallib.php
+++ plugins/subject/locallib.php
@@ -49,8 +49,10 @@ class progressreview_subject extends progressreview_subject_template {
         global $DB;
         $grades = (array)parent::retrieve_targetgrades($items);
         $studentid = $this->progressreview->get_student()->id;
-        if($avgcse = $DB->get_record('user_info_data', array('fieldid' => 1, 'userid' => $studentid))) {
-            $grades['min'] = $avgcse->data;
+        if ($this->progressreview->get_session()->inductionreview) {
+            if($avgcse = $DB->get_record('user_info_data', array('fieldid' => 1, 'userid' => $studentid))) {
+                $grades['min'] = $avgcse->data;
+            }
         }
         return (object)$grades;
     } // end of member function retrieve_targetgrade
