564dc13f9065f9a17da67ae8b53ad577c8910398
Mark Johnson <mark.johnson@tauntons.ac.uk>
2011-11-24 09:47:47 +0000

    Prevented Didn't Attend button from displaying twice
    If saving and displaying 2 different progressreviews on the same page load,
    the init function will be called twice.  This stops it doing anything.

--- plugins/tutor/module.js
+++ plugins/tutor/module.js
@@ -1,15 +1,19 @@
 M.progressreview_tutor = {
 
     init: function(Y) {
-        var strdidntattend = M.util.get_string('didntattend', 'progressreview_tutor');
-        var button = Y.Node.create('<button id="didntattend">'+strdidntattend+'</button>');
-        button.on('click', function(e) {
-            e.preventDefault();
-            comments = e.target.siblings('#id_comments');
-            commentsvalue = comments.get('value');
-            strfiller = M.util.get_string('didntattendfiller', 'progressreview_tutor');
-            comments.set('value', strfiller+' '+comments.get('value'));
+        if (!Y.one('#didntattend')) {
+            var strdidntattend = M.util.get_string('didntattend', 'progressreview_tutor');
+            var button = Y.Node.create('<button id="didntattend">'+strdidntattend+'</button>');
+            button.on('click', function(e) {
+                e.preventDefault();
+                comments = e.target.siblings('#id_comments');
+                commentsvalue = comments.get('value');
+                strfiller = M.util.get_string('didntattendfiller', 'progressreview_tutor');
+                comments.set('value', strfiller+' '+comments.get('value'));
+                Y.one('#id_comments').simulate('change');
+            });
+            Y.one('#id_comments').insert(button, 'after');
+        }
         });
-        Y.one('#id_comments').insert(button, 'after');
     }
 }
