4b74e3a3fa5c5bf09bf8eccb83cf63570d524d2d
Mark Johnson <mark.johnson@tauntons.ac.uk>
2011-11-29 09:15:17 +0000

    Improved error handling and logging of autosave script

--- autosave.php
+++ autosave.php
@@ -18,44 +18,41 @@ try {
     progressreview_controller::validate_student($studentid);
     progressreview_controller::validate_course($courseid);
     progressreview_controller::validate_teacher($teacherid);
+
+    require_login($courseid);
+
+    $coursecontext = get_context_instance(CONTEXT_COURSE, $courseid);
+
+    require_capability('moodle/local_progressreview:write', $coursecontext);
+
+    $progressreview = current(progressreview_controller::get_reviews($sessionid,
+                                                                     $studentid,
+                                                                     $courseid,
+                                                                     $teacherid,
+                                                                     $type));
+
+    $plugin = $progressreview->get_plugin($plugin);
+
+    $plugin->autosave($field, $value);
+
 } catch (moodle_exception $e) {
+    add_to_log($courseid, 'local_progressreview', 'update failed', '', get_class($e));
     header('HTTP/1.1 400 Bad Request');
     die($e->getMessage());
-}
-
-try {
-    require_login($courseid);
-} catch (require_login_exception $e) {
-    header('HTTP/1.1 403 Forbidden');
-    die($e->getMessage());
-}
-
-$coursecontext = get_context_instance(CONTEXT_COURSE, $courseid);
-
-try {
-    require_capability('moodle/local_progressreview:write', $coursecontext);
-} catch (required_capability_exception $e) {
-    header('HTTP/1.1 403 Forbidden');
-    die($e->getMessage());
-}
-
-$progressreview = current(progressreview_controller::get_reviews($sessionid,
-                                                                 $studentid,
-                                                                 $courseid,
-                                                                 $teacherid,
-                                                                 $type));
-
-$plugin = $progressreview->get_plugin($plugin);
-
-try {
-    $plugin->autosave($field, $value);
-} catch (progressreview_autosave_exception $e) {
-    header('HTTP/1.1 500 Internal Server Error');
-    die($e->getMessage());
 } catch (progressreview_invalidfield_exception $e) {
+    add_to_log($courseid, 'local_progressreview', 'update failed', '', get_class($e));
     header('HTTP/1.1 400 Bad Request');
     die($e->getMessage());
 } catch (dml_write_exception $e) {
+    add_to_log($courseid, 'local_progressreview', 'update failed', '', get_class($e));
     header('HTTP/1.1 400 Bad Request');
-    die($e->error);
+    die($e->getMessage());
+} catch (require_login_exception $e) {
+    add_to_log($courseid, 'local_progressreview', 'update failed', '', get_class($e));
+    header('HTTP/1.1 403 Bad Request');
+    die($e->getMessage());
+} catch (required_capability_exception $e) {
+    add_to_log($courseid, 'local_progressreview', 'update failed', '', get_class($e));
+    header('HTTP/1.1 403 Bad Request');
+    die($e->getMessage());
 }
