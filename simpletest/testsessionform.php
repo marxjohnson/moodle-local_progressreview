2b46220fa64c6552c8e86b597b3ce4144db5a179
Mark Johnson <mark.johnson@tauntons.ac.uk>
2012-01-26 15:25:15 +0000

    Added test for progressreview_session_form::process() method

--- simpletest/testsessionform.php
+++ simpletest/testsessionform.php
@@ -15,7 +15,7 @@ class sessionform_test extends UnitTestCaseUsingDatabase {
     public function setUp() {
         $this->switch_to_test_db();
         $this->create_test_table('config_plugins', 'lib');
-        $this->create_test_table('progressreview_session', 'local/progressreview');
+        $this->create_test_tables(array('progressreview_session', 'progressreview_activeplugins'), 'local/progressreview');
         $this->testdata['config_plugins'] = $this->load_test_data('config_plugins',
             array('plugin', 'name', 'value'), array(
             array('progressreview_subject', 'version', 1234),
@@ -56,6 +56,61 @@ class sessionform_test extends UnitTestCaseUsingDatabase {
         $form = new progressreview_session_form();
 
         $this->assertEqual($method->invoke($form), array('subject', 'targets', 'tutor'));
+
+    }
+
+    public function test_process() {
+        global $CFG;
+        require_once($CFG->dirroot.'/local/progressreview/lib.php');
+        $subjectdeadline = time()+(rand(1,4)*TIME_ONEWEEK);
+        $tutordeadline = $subjectdeadline+(TIME_ONEWEEK);
+        $form = new progressreview_session_form();
+        $data = (object)array(
+            'name' => 'Progress Review 2',
+            'deadline_subject' => $subjectdeadline,
+            'deadline_tutor' => $tutordeadline,
+            'lockafterdeadline' => 0,
+            'scale_behaviour' => 'Good,Bad,Ugly',
+            'scale_effort' => 'Good,Bad,Ugly',
+            'scale_homework' => 'Good,Bad,Ugly',
+            'template_subject' => null,
+            'template_tutor' => null,
+            'snapshotdate' => null,
+            'previoussession' => null,
+            'inductionreview' => 0,
+            'plugins' => array(
+                'subject' => 1,
+                'tutor' => 1,
+                'targets' => 0
+            )
+        );
+
+        $record = clone($data);
+        unset($record->plugins);
+        $record->id = $form->process($data);
+
+        $sessionrecord = $DB->get_record('progressreview_session', array('id' => $record->id));
+        $pluginrecords = $DB->count_records('progressreview_activeplugins', array('sessionid' => $record->id));
+        $subjectpluginparams = array(
+            'plugin' => 'subject',
+            'sessionid' => $record->id,
+            'reviewtype' => PROGRESSREVIEW_SUBJECT
+        );
+        $tutorpluginparams = array(
+            'plugin' => 'tutor',
+            'sessionid' => $record->id,
+            'reviewtype' => PROGRESSREVIEW_TUTOR
+        );
+        $targetpluginparams = array(
+            'plugin' => 'target',
+            'sessionid' => $record->id,
+            'reviewtype' => PROGRESSREVIEW_TUTOR
+        );
+        $this->assertEqual($record, $sessionrecord);
+        $this->assertEqual($pluginrecords, 2);
+        $this->assertTrue($DB->record_exists('progressreview_activeplugins', $subjectpluginparams));
+        $this->assertTrue($DB->record_exists('progressreview_activeplugins', $tutorpluginparams));
+        $this->assertFalse($DB->record_exists('progressreview_activeplugins', $targetpluginparams));
 
     }
 
